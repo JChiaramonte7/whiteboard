9efa356009fff2eeb7057b2b24c5b53125c501f2
BuildTools <unconfigured@null.spigotmc.org>
2015-07-25 22:02:45 -0400

    Second Push

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ $(document).ready(function(){
         var editor = ace.edit("editor");
         var theme = $("#theme").val();
         editor.setTheme("ace/theme/" + theme);
-    }
-    )
-}
-)
+    });
+    $("#mode").on('change',function(){
+        var editor = ace.edit("editor");
+        var mode = $("#mode").val();
+        editor.getSession().setMode("ace/mode/" + mode);
+    });
+});
